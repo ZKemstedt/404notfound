d54ca8ea85501325bdddc6b61f87b0eb6c5c8eb7
Zephyro Kemstedt <zkemstedt@gmail.com>
2020-12-04 14:12:45 +0100

    pause_menu -> game.py

--- game.py
+++ game.py
@@ -1,3 +1,15 @@
 # Game loop
 #
 # 
+
+
+
+def pause_menu():
+    choice = input('Do you want to 1: Exit the game or 2: Continue?')
+    if (choice == 1):
+        break
+    elif (choice == 2):
+        continue
+    else:
+        print('Try again')
+        pass
