75606b057c52e121d44cf05634539309bdc9a993
hallonpie <kazisadman.rashid@yh.nackademin.se>
2020-12-04 02:02:57 +0100

    create charecter and power

---
+++ create_character.py
@@ -0,0 +1,51 @@
+import random
+
+
+class Character(object):
+
+    def __init__(self, name, initiative, health, power, evasion):
+        self.name = name
+        self.initiative = initiative
+        self.health = health
+        self.power = power
+        self.evasion = evasion
+
+    def special_power(self):
+        raise NotImplementedError('Must be implemented!')
+
+
+class Knight(Character):
+
+    def __init__(self, name):
+        super().__init__(name, 5, 9, 6, 4)
+
+    def special_power(self):
+        print('As a Knight you skip first attack from monster')
+
+
+class Wizard(Character):
+
+    def __init__(self, name):
+        super().__init__(name, 6, 4, 9, 5)
+
+    def special_power(self):
+        escape_chance = (random.randit(1, 100))
+        if escape_chance <= 80:
+            print('Escaped')
+            return True
+        else:
+            return False
+
+
+class Thief(Character):
+
+    def __init__(self, name):
+        super().__init__(name, 7, 5, 5, 7)
+
+    def special_power(self):
+        critical_hit = (random.randit(1, 100))
+        if critical_hit <= 25:
+            print('Escaped')
+            return True
+        else:
+            return False
