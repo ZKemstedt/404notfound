019755b3e27fd98e702b39b5df0f38f54106934f
Zephyro Kemstedt <zkemstedt@gmail.com>
2020-12-04 12:35:08 +0100

    main menu template file

---
+++ __main__.py
@@ -0,0 +1,12 @@
+
+# Launcher
+# 
+# Menus and interactions before the game starts and after the game ends
+# 
+# Main Menu
+# Load character
+# Save character
+#
+# < these can go in either game.py or __main__.py, you get to choose :) >
+# Select boardsize ?
+# Select startposition ?
