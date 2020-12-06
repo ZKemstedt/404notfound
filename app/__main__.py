ee2f9497945dbe6e3873aecde895a4b64cc39db1
Ludvig <ludvig.jerner@yh.nackademin.se>
2020-12-06 13:52:02 +0100

    commit main menu

--- app/__main__.py
+++ app/__main__.py
@@ -7,3 +7,36 @@
 # Load character
 # Save character
 #
+
+# Dungeon Run
+
+def main_menu():
+    print("""
+     ___________                                                                           __
+    |    _ _    \   ___    ___  ___       __    ________   ________  ___    ___  ___      |  |
+    |   |   \    \ |   |  |   ||    \    |  |  /   _____\ |   _____||   |  |   ||    \    |  |
+    |   |    |    ||   |  |   ||     \   |  | /  /   ____ |  |____  |   |  |   ||     \   |  |
+    |   |   /    / |   |  |   ||  |\   \ |  ||   |  |_  | |   ____| |   |  |   ||  |\   \ |  |
+    |   - -    /   |   |__|   ||  |  \  \|  | \   \__/  / |  |_____ |   |__|   ||  |  \  \|  |
+     ----------     \________/ |__|   \_____|   \______/  |________| \________/ |__|   \_____|
+                               ______    ___    ___  ___       __ 
+                              |   _   \ |   |  |   ||    \    |  |
+                              |  |  |  ||   |  |   ||     \   |  |
+                              |   --  / |   |  |   ||  |\   \ |  |
+                              |  |\   \ |   |__|   ||  |  \  \|  |
+                              |__| \___\ \________/ |  |   \_____| 
+                                                    |__|
+        """)
+    try:
+        menu_choice = int(input("""
+                                    [ 1 ] Load Character\n
+                                    [ 2 ] Save Character\n
+                                    [ 3 ] Exit Game
+                            
+                                    Make a move: """))
+        if menu_choice == 3:
+            return exit()
+        else:
+            return menu_choice
+    except ValueError:
+        print('Ops, you can only enter an integer!')
