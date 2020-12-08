4ac3ac0ec0c6a2f92d35b70a7c1ca7f43a07e83d
Zephyro Kemstedt <zkemstedt@gmail.com>
2020-12-08 09:09:58 +0100

    Treasure: capitalize class names

--- app/treasure.py
+++ app/treasure.py
@@ -5,31 +5,31 @@ class Treasure(object):
         self.appearance_rate = appearance_rate
 
 
-class Coins(treasure):
+class Coins(Treasure):
 
     def __init__(self):
         super().__init__(2, 0.4)
 
 
-class Pouch(treasure):
+class Pouch(Treasure):
 
     def __init__(self):
         super().__init__(6, 0.2)
 
 
-class GoldJewelry(treasure):
+class GoldJewelry(Treasure):
 
     def __init__(self):
         super().__init__(10, 0.15)
 
 
-class Gemstone(treasure):
+class Gemstone(Treasure):
 
     def __init__(self):
         super().__init__(14, 0.1)
 
 
-class SmallTreasureChest(treasure):
+class SmallTreasureChest(Treasure):
 
     def __init__(self):
         super().__init__(20, 0.05)
