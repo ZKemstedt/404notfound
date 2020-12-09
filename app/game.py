26bb9fd5eec07947cf73d3ee938b63469271dcff
Zephyro Kemstedt <zkemstedt@gmail.com>
2020-12-09 14:50:19 +0100

    add game loop and templates

--- app/game.py
+++ app/game.py
@@ -10,6 +10,77 @@
 #
 #   Exit
 #
+from typing import List, Union
+
+from app.board import Board, Tile
+from app.monster import Monster
+from app.character import Character
+
+
+def game_loop(board: Board, tile: Tile) -> bool:
+    """Game Flow - Game Loop
+
+    Args:
+        board (Board): The board the game is played on
+        tile (Tile): The players starting tile
+
+    Returns:
+        bool: True if user cleared the board, False if the user died
+    """
+    game_run = True
+    while game_run:
+        target = player_move_menu()  # target: Tile or None
+
+        if target is None:  # Sudden Game Exit
+            return False  # Do not save
+
+        if target.monsters:
+            isalive = battle(player=tile.player, monsters=target.monsters)
+            if not isalive:
+                return False
+
+        if target.treasure:
+            pass  # player pickup treasure
+
+        if target.exit:
+            # exit yes or no?
+            # and then return True
+            return True
+
+        move_player(tile, target)
+
+
+def battle(player: Character, monsters: List[Monster]) -> bool:
+    """Game Flow - Battle Loop
+
+    Args:
+        player (Character): [description]
+        monsters (list: [description]
+
+    Returns:
+        bool: is the user still alive?
+    """
+    pass
+
+
+def player_move_menu() -> Union[Tile, None]:
+    """[summary]
+
+    Returns:
+        Tile: The tile the player is trying to move towards
+        None: The player decided to stop playing
+    """
+    pass
+
+
+def move_player(old: Tile, new: Tile) -> None:
+    """Move the player object from Tile `old` to Tile `new`
+
+    Args:
+        old (Tile): [description]
+        new (Tile): [description]
+    """
+    pass
 
 
 # To be merged into Game Loop Menu / Player Move Loop
