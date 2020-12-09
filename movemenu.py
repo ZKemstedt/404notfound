e250c43d0281628dbb8ef0dd83655a6d8f237fe4
Drylum <josv2001@yh.nackademin.se>
2020-12-09 11:51:53 +0100

    Test move menu

---
+++ movemenu.py
@@ -0,0 +1,13 @@
+move = input('Which way do you want to move?\nW: Up, A: Left, S: Down, D: Right\n')
+while True:
+    if (move == w):
+        pass
+    elif (move == a):
+        pass
+    elif (move == s):
+        pass
+    elif (move == d):
+        pass
+    else:
+        print('Not a valid choice, try again')
+        return
