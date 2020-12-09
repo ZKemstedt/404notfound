b2c86239b4b9adb0cad189d37add490c1417a7bc
Drylum <josv2001@yh.nackademin.se>
2020-12-09 16:51:08 +0100

    Something like this?

--- movemenu.py
+++ movemenu.py
@@ -1,13 +1,20 @@
-move = input('Which way do you want to move?\nW: Up, A: Left, S: Down, D: Right\n')
-while True:
-    if (move == w):
-        pass
-    elif (move == a):
-        pass
-    elif (move == s):
-        pass
-    elif (move == d):
-        pass
-    else:
-        print('Not a valid choice, try again')
-        return
+def moveMenu():
+    move = input('Which way do you want to move?\nW: Up, A: Left, S: Down, D: Right\n')
+    while True:
+     for x in range(MAPWIDTH):
+         for y in range(MAPHEIGHT):
+             if tile[x][y] == wall:
+                 print('Wall, try another way!')
+                 pass
+
+                if move.lower() == 'w':
+                    tile[y+1]
+                elif move.lower() == 'a':
+                    tile[x-1]
+                elif move.lower() == 's':
+                    tile[y-1]
+                elif move.lower() == 'd':
+                    tile[x+1]
+                else:
+                    print('Not a valid choice, try again')
+                    return
