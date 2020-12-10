f6b79e42ef5f5555898ea36d8a12d2b5fa785b17
marcus <me@example.com>
2020-12-10 11:19:10 +0100

    battle_m

--- app/battle_menu.py
+++ app/battle_menu.py
@@ -1,36 +1,21 @@
 import character
 import monster
+import board
+import random
 
 
-def battle_menu(player, monster):
-    menu_choice = print_btl_menu(player, monster)
-    if menu_choice == '1':
-        print('attack')
-        #Choose which monster to attack
-    elif menu_choice == '2':
-        print('flee')
-        #Attempt flee
-    elif menu_choice == '3':
-        print('exit game')
-    else:
-        print('Invalid command, try again from the menu')
-#How do we treat two monsters on same tile?
-
-
-def print_btl_menu(player, monster):
+def print_btl_menu(player): # need to add monsters
     name_list = [[]]
     #Temporary storing in list to get the print menu to work
     name_list[0].append('Zombie')
-    a = monster.health
+    #a = monster.health
     #b = monster.name
     name_list[0].append(1)
     name_list.append([])
-    name_list[1].append('Troll')
-    name_list[1].append(2) 
     #Everything above this needs to be changed
     full_str = ''
     monster = 2
-    for _ in range(monster):
+    for _ in range(1): # in range monster
         full_str += name_list[_][0] + '\t\t' + str(name_list[_][1]) + '\n'
     full_str = full_str[:-1]    #remove last NewLine
     
@@ -45,6 +30,40 @@ Name\t\tHealth
     return menu_choice
 
     
-battle_menu(character.Character('Elsa', 15, 1, 1, 1), monster.Monster(2, 3, 3, 4, 0.15))
-ab = monster.Monster(2, 3, 3, 4, 0.15)
+elsa = character.Character('Elsa', 15, 1, 4, 15)
+abbe = character.Character('micke', 11, 2, 7, 9)
+monster = monster.Monster(2, 3, 3, 4, 0.15)
 
+
+def dice(x) -> int:
+    dice_sum = 0
+    for _ in range(1,x+1):
+        print('_ ====' , _)
+        dice_sum += random.randint(1,6)
+        print(dice_sum)
+    return dice_sum
+
+
+tile = board.Tile(0,0)
+x = tile.monsters
+x.append('scarymonsta')
+x.append(1)
+
+
+def fight(player):
+    cmd = print_btl_menu(elsa)
+    #Player has entered fight and chosen 1, (fight!)
+    if(cmd == '1'):
+        
+        attack_value = dice(player.power)
+        #attack value is the dice_sum, dice thrown for each point of power
+
+    
+def fighting(attacker, defender):
+    print('axax')
+    
+    
+    
+
+
+fight(elsa)
