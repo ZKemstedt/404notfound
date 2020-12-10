159d06cd495d046d55d4aa418adb18ad0dd08031
Ludvig <ludvig.jerner@yh.nackademin.se>
2020-12-10 15:01:04 +0100

    ändrade utseendet på tiles

--- app/board.py
+++ app/board.py
@@ -18,11 +18,11 @@ class Tile:
 
     def __str__(self) -> str:
         if self.player:
-            return 'P'
+            return '[P]'
         elif self.explored:
-            return '/'
+            return '[/]'
         else:
-            return 'X'
+            return '[X]'
 
     def place_character(self, player):
         self.player = player
@@ -83,3 +83,4 @@ if __name__ == "__main__":
     target = board.get_tile((1, 3))
     target.place_character(example_player)
     print(board)
+    
