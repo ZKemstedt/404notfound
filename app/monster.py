049a023c178d0abde7879880f17423bd28be6381
Amir <amir.jawhari.92@gmail.com>
2020-12-12 02:02:20 +0100

    Removed appearance_rate attribute

--- app/monster.py
+++ app/monster.py
@@ -1,17 +1,16 @@
 class Monster(object):
 
-    def __init__(self, health, evasion, power, initiative, appearance_rate):
+    def __init__(self, health, evasion, power, initiative):
         self.health = health
         self.evasion = evasion
         self.power = power
         self.initiative = initiative
-        self.appearance_rate = appearance_rate
 
 
 class GiantSpider(Monster):
 
     def __init__(self):
-        super().__init__(1, 3, 2, 7, 0.2)
+        super().__init__(1, 3, 2, 7)
 
     def __str__(self) -> str:
         return 'GiantSpider'
@@ -20,7 +19,7 @@ class GiantSpider(Monster):
 class Skeleton(Monster):
 
     def __init__(self):
-        super().__init__(2, 3, 3, 4, 0.15)
+        super().__init__(2, 3, 3, 4)
 
     def __str__(self) -> str:
         return 'Skeleton'
@@ -29,7 +28,7 @@ class Skeleton(Monster):
 class Orc(Monster):
 
     def __init__(self):
-        super().__init__(3, 4, 4, 6, 0.1)
+        super().__init__(3, 4, 4, 6)
 
     def __str__(self) -> str:
         return 'Orc'
@@ -38,7 +37,7 @@ class Orc(Monster):
 class Troll(Monster):
 
     def __init__(self):
-        super().__init__(4, 2, 7, 2, 0.05)
+        super().__init__(4, 2, 7, 2)
 
     def __str__(self) -> str:
         return 'Troll'
